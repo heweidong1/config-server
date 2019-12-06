e9cadd1206f952f4478de12774e314e30bfedfb0
heweidong1 <a18233117759@163.com>
2019-12-06 09:12:36 +0800

    Initial commit

---
+++ src/test/java/com/kgc/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgc;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
